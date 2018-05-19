c7e0806c6d879a6e4ecbe1c20d8fe1341c74c2b8
Maciej Motak <mmotak@fp-mac1863.local>
2018-05-19 14:11:16 +0200

    add data source

---
+++ Todoey/DataSource.swift
@@ -0,0 +1,21 @@
+//
+//  DataSource.swift
+//  Todoey
+//
+//  Created by Maciej Motak on 19/05/2018.
+//  Copyright © 2018 Maciej Motak. All rights reserved.
+//
+
+import Foundation
+
+public class DataSource {
+    private let list = ["zadanie 1", "zadanie 2", "zadanie 3"]
+    
+    public func size() -> Int {
+        return list.count
+    }
+    
+    public func getString(_ index: Int) -> String {
+        return list[index]
+    }
+}
--- Todoey/TableMainViewController.swift
+++ Todoey/TableMainViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class TableMainViewController: UITableViewController {
-
+    let dataSource = DataSource();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,17 @@ class TableMainViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.size()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = dataSource.getString(indexPath.row)
+        
+        return cell
+    }
 }
 
