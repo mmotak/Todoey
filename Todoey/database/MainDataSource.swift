4264cbc5516c74aecb0ea0ad591f3bd0500e6d13
Maciej Motak <mmotak@snip-macbook-pro.local>
2018-08-18 14:16:43 +0200

    added realm pods

---
+++ Todoey/database/MainDataSource.swift
@@ -0,0 +1,17 @@
+//
+//  MainDataSource.swift
+//  Todoey
+//
+//  Created by Maciej Motak on 18.08.2018.
+//  Copyright © 2018 Maciej Motak. All rights reserved.
+//
+
+import Foundation
+
+public class MainDataSource {
+    // This class will have the all data source inside
+    // files
+    // plist
+    // datacore
+    // realm
+}
